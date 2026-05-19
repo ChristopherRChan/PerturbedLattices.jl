3045fc0a8f38c9dd5cebc71cc15d543644a9ac8e
R cqls <rdrouilh@gmail.com>
2026-05-19 19:03:18 +0200

    Update hamiltonian.jl

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -41,49 +41,7 @@ function pointset!(hch::HardCoreHamiltonian, ps::AbstractPointSet)
     hch.pointset = convert(PointSet, ps)
     update!(hch.pointset)
 end
-
-# local_energy(h::StraussHamiltonian, i::Int) = h.beta * sum(h.adjacency[i, :])
-
-# local_energy(h::HardCoreHamiltonian, i::Int) = any(h.adjacency[i, :]) ? Inf : 0.0
-
+ 
 local_energy(h::StraussHamiltonian, i::Int) = h.beta * sum(square_distance(h.pointset)[i, :] .<= h.radius^2)
 
 local_energy(h::HardCoreHamiltonian, i::Int) = any(h.adjacency[i, :] .<= h.radius^2) ? Inf : 0.0
-
-
-# function adjacency_matrix!(h::AbstractHamiltonian)
-#     points = h.pointset.points
-#     n_points = length(points)
-#     adjacency = falses(n_points, n_points)
-
-#     for i in 1:n_points
-#         for j in (i+1):n_points
-#             dist_sq = sum((points[i] .- points[j]).^2)
-#             if dist_sq <= h.radius^2
-#                 adjacency[i, j] = adjacency[j, i] = true
-#             end
-#         end
-#     end
-
-#     h.adjacency = adjacency
-# end
-
-
-# function adjacency_matrix!(h::AbstractHamiltonian, i::Int, new_point::Point)
-#     points = h.pointset.points
-#     n_points = length(points)
-
-#     # Réinitialiser la ligne et la colonne i
-#     h.adjacency[i, :] .= h.adjacency[:, i] .= false
-
-#     # Recalculer les adjacences pour le point i à sa nouvelle position
-#     for j in 1:n_points
-#         if j != i
-#             dist_sq = sum((new_point .- points[j]).^2)
-#             if dist_sq <= h.radius^2
-#                 h.adjacency[i, j] = h.adjacency[j, i] = true
-#             end
-#         end
-#     end
-
-# end
